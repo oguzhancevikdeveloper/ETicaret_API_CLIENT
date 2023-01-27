519dc4ca325f09ce9e82a5c5511ae3c59d702afe
Oguzz <Oguzz@OGUZHAN_PC>
2023-01-27 16:34:49 +0300

    FluenValidation

--- ETicaretAPI.API/Program.cs
+++ ETicaretAPI.API/Program.cs
@@ -1,4 +1,7 @@
+﻿using ETicaretAPI.Application.Validators.Products;
+using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPI.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +9,12 @@ builder.Services.AddPersistenceServices();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers
+    (options => options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation
+    (configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+    .ConfigureApiBehaviorOptions
+    (options => options.SuppressModelStateInvalidFilter = true); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
 
---
+++ ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,38 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen, Ürün Adını Boş Geçmeyiniz!")
+                .MaximumLength(150)
+                .MinimumLength(3)
+                .WithMessage("Lütfe, Ürün Adını 3 ile 150 Karakter Arasında Giriniz");
+
+            RuleFor(s => s.Stock)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen, Stok Bilgisini Boş Geçmeyiniz")
+                .Must(s => s >= 0)
+                .WithMessage("Lütfen, Stok Bilgisini 0' dan Büyük Giriniz");
+
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen, Fiyat Bilgisini Boş Geçmeyiniz")
+                .Must(p => p >= 0)
+                .WithMessage("Lütfen, Fiyat Bilgisini 0' dan Büyük Giriniz");
+        }
+    }
+}
